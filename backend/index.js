c1616aae7eccfb3338ffff5605d0b74006b453aa
A-rya-n <aryan31.purohit@gmail.com>
2023-07-20 15:14:20 +0530

    Separated code into routes,controllers

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const parser = require("body-parser");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 const PORT = 3000;
@@ -8,10 +9,7 @@ const PORT = 3000;
 app.use(cors());
 app.use(parser.json());
 
-app.post("/user/register", (req, res) => {
-  console.log("Recieved registration info: ", req.body);
-  res.status(200).json({ message: "Registration successful" });
-});
+app.use("/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);

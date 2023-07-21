f539fc02e78c95a58fc9b7188dcfa5b27f871a54
A-rya-n <aryan31.purohit@gmail.com>
2023-07-21 11:27:43 +0530

    Database connected and sending info

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const parser = require("body-parser");
+require('dotenv').config();
+const mongoose = require("mongoose");
+
 const userRouter = require("./routes/userRoutes");
 
 const app = express();
@@ -9,6 +12,16 @@ const PORT = 3000;
 app.use(cors());
 app.use(parser.json());
 
+const connectDB = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.DB_CONNECTION);
+    console.log("Database connected!");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+connectDB();
 app.use("/user", userRouter);
 
 app.listen(PORT, () => {
---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: [true, "E-mail already taken."],
+    },
+    user: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);

caa6bfdd4f52d8e974228ee023b9bae11625c1dd
A-rya-n <aryan31.purohit@gmail.com>
2023-07-24 12:21:29 +0530

    Accessing profile with token

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,13 +5,18 @@ import { HomeComponent } from './home/home.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AuthGuardService } from './shared/services/auth-guard.service';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'registration', component: RegistrationComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'profile', component: ProfileComponent },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AuthGuardService],
+  },
 ];
 
 @NgModule({
--- client/src/app/shared/services/auth-guard.service.ts
+++ client/src/app/shared/services/auth-guard.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
-
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
+import jwt_decode from 'jwt-decode';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      const decodedToken: any = jwt_decode(token);
+      const currentTime = Date.now() / 1000;
+
+      if (decodedToken.exp > currentTime) {
+        return true;
+      } else {
+        this.router.navigate(['/home']);
+        return false;
+      }
+    } else {
+      this.router.navigate(['/home']);
+      return false;
+    }
+  }
 }
